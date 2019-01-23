3b4ea05938ba0608af7e2cc79f7ac4a3c680f2af
din1122 <dinashkenazi@gmail.com>
2019-01-23 14:36:21 +0200

    Initial commit

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -6,32 +6,27 @@
     <v-card
       flat
       tile
-      class="footer-bg text-xs-center"
+      class="footer text-xs-center"
     >
       <v-card-text>
-    <img :src="require('../assets/white-logo.png')" alt="idanim-group-white-logo" class="footer-logo">   
+     
       </v-card-text>
 
       <v-card-text class="white--text pt-0">
-       קבוצת עידנים רואה את נושא השירות ואיכות העבודה כיעד ראשון.
-החברות השונות מעסיקות מנהלי עבודה שכל מטרתם היא לפקח ולייעל את איכות העבודה והשרות שהקבוצה מעניקה ללקוחות השונים.
-עידנים עבודות ציבוריות בע"מ מפעילה רכבי טיאוט חדישים בגדלים שונים, מעסיקה מכונאים, מנהלים ועובדים מיומנים והכל בכדי לוודא שהעבודה תתנהל ברמה הגבוהה ביותר.
-עידנים תשתיות בע"מ המשווקת מטאטאי כביש וציוד ניקיון נוסף, מפעילה מערך שירות של מכונאים בעלי נסיון של שנים רבות.
-לרשות החברה עומד מוסך מצוייד היטב, המאוייש במכונאים ואנשי שירות ותיקים ומנוסים.
-בנוסף, מפעילה החברה ניידת שירות המאובזרת בציוד וכלים הטובים ביותר ומשמשת מעין מוסך נייד,
-כמו כן יש בניידת השירות מגוון עצום של חלקים המאפשר לבצע את הטיפולים והתיקונים הנדרשים במקום הימצאו של הכלי ובזמן הקצר ביותר. 
- החברה מתחייבת להעניק שירות וטיפול ללא הגבלה לשביעות רצון הלקוח.
+       Medical cannabis,is cannabis and cannabinoids that are recommended by doctors for their patients. Evidence suggests that cannabis can reduce nausea and vomiting during chemotherapy, improve appetite in people with HIV/AIDS, and reduce chronic pain and muscle spasms.The Cannabis plant has a history of medicinal use dating back thousands of years in many cultures .Medical cannabis can be administered through a variety of methods, including capsules, lozenges, tinctures, dermal patches, oral or dermal sprays, cannabis edibles, and vaporizing or smoking dried buds.
       </v-card-text>
 
       <v-divider></v-divider>
 
-      <v-card-text class="white--text" style="direction:ltr;">
-        &copy;2019 — <strong>Idanim Group</strong>
+      <v-card-text class="white--text">
+        &copy;2019 — <strong>Cannadis</strong>
       </v-card-text>
     </v-card>
   </v-footer>
 </template>
 
 <style scoped>
-
+.footer{
+    background: linear-gradient(180deg,#78B821 79%,#34401e  136%);
+}
 </style>
